94b21606e36380038c966e3ec6514717b2ff04be
shsyss <103440382+shsyss@users.noreply.github.com>
2022-06-12 19:07:48 +0900

    end sort.rs && update setting.toml comment

--- src/sort.rs
+++ src/sort.rs
@@ -3,10 +3,13 @@ pub fn sorting(
     read_filename_extension: &str,
     ex_filename: Vec<String>,
 ) -> Vec<String> {
+    // 拡張子で指定
     let filenames = filenames
         .into_iter()
-        .filter(|f| f.ends_with(read_filename_extension))
-        .collect::<Vec<_>>();
+        .filter(|f| f.ends_with(read_filename_extension));
 
+    // 除外するファイル名を指定
     filenames
+        .filter(|f| ex_filename.iter().all(|ef| !f.starts_with(ef)))
+        .collect::<Vec<_>>()
 }
