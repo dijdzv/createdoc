00cb87c20ac9b12ec2d93c4f8c725791e4c69a1c
shsyss <103440382+shsyss@users.noreply.github.com>
2022-06-22 22:29:10 +0900

    search_dataをとりあえず返した

--- src/create/search/script/data.rs
+++ src/create/search/script/data.rs
@@ -1,10 +1,22 @@
 use crate::FolderVec;
+use std::collections::HashMap;
 
 pub fn search_data(folder_vec: &FolderVec) -> String {
-    let filenames = folder_vec
-        .iter()
-        .map(|(filename, _)| filename.as_str())
-        .collect::<Vec<_>>();
+    let mut hashmap = HashMap::new();
 
-    filenames.join(" ")
+    for (filename, file_vec) in folder_vec {
+        let target_names = file_vec
+            .iter()
+            .map(|(t, _, _)| t.as_str())
+            .collect::<Vec<_>>();
+
+        hashmap.insert(&filename[..filename.find('.').unwrap()], target_names);
+    }
+    let mut buf = Vec::new();
+    for (k, v) in hashmap {
+        buf.push(format!("{}: [{}]", k, v.join(",")));
+    }
+    let hash_str = buf.join(",\n");
+
+    format!("const search_data = {{\n {}\n }}", hash_str)
 }
