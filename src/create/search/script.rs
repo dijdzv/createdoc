de5c9f90475c4da0a3de73726ab5682682e72588
shsyss <103440382+shsyss@users.noreply.github.com>
2022-06-23 22:36:16 +0900

    inputに文字が入力されているときにmarginTopを付与

--- src/create/search/script.rs
+++ src/create/search/script.rs
@@ -7,14 +7,14 @@ document.addEventListener('keyup', (e) => {
 });
 searchInput.addEventListener('input',()=>{
     let v = searchInput.value;
-    let isFirst = true;
+    if (v){
+      searchInput.style.marginBottom = '1rem';
+    }else{
+      searchInput.style.marginBottom = '0';
+    }
     for (let i = 0; i < searchData.length; i++){
      if (searchData[i].indexOf(v) != -1 && v){
         searchList[i].classList.remove('dn');
-        if (isFirst) {
-          searchList[i].style.marginTop = '1rem'
-          isFirst = false;
-        }
      }else{
         searchList[i].classList.add('dn');
      }
