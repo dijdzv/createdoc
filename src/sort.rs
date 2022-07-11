ea2b64e35128a754d00d962c2862183adf562608
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-11 14:20:51 +0900

    partical_cmp から cmpに

--- src/sort.rs
+++ src/sort.rs
@@ -4,7 +4,7 @@ pub fn sort(folder_vec: &mut FolderVec) -> FolderVec {
     folder_vec
         .iter_mut()
         .map(|(f, file_vec)| {
-            file_vec.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+            file_vec.sort_by(|a, b| a.0.cmp(&b.0));
             (f.to_owned(), file_vec.clone())
         })
         .collect::<Vec<_>>()
