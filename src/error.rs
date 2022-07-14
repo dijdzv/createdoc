5d4d267a57fefd901bf1c73ecebd9f72342b7032
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-14 10:39:15 +0900

    errormsg update

--- src/error.rs
+++ src/error.rs
@@ -11,10 +11,10 @@ impl ErrorMsg {
     pub fn as_str(&self) -> &'static str {
         use ErrorMsg::*;
         match *self {
-            Captures => "dev: Regex.captures() error",
+            Captures => "Contains invalid characters. [character,digit,_]",
             FileName => "The loaded path is terminated with `..`.",
             FileStem => "There is no file name.",
-            Get => "dev: Captures.get() error",
+            Get => "Failed to obtain target name.",
             Parent => "The loaded Path ends with root or prefix.",
             ToStr => "The loaded file name is not in valid Unicode.",
         }
