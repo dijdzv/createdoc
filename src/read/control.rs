9629c6205f17882f0fa7a4f52155eb5cf9a93249
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-17 22:34:00 +0900

    exclude_filenameをexclude_fileに

--- src/read/control.rs
+++ src/read/control.rs
@@ -5,28 +5,28 @@ use std::path::Path;
 fn read_recursive<P: AsRef<Path>>(
     path: P,
     ext: &str,
-    exclude_filename: &[String],
+    exclude_file: &[String],
     read_folder: &[String],
     current: &mut Vec<String>,
 ) -> io::Result<()> {
     if !path.as_ref().exists() {
         return Ok(());
     }
-    let (mut read_filename, exist_folder) = read_dir(&path, ext, exclude_filename)?;
+    let (mut read_filename, exist_folder) = read_dir(&path, ext, exclude_file)?;
     current.append(&mut read_filename);
     if exist_folder.is_empty() {
         return Ok(());
     }
     if read_folder.contains(&"*".to_string()) {
         for f in &exist_folder {
-            read_recursive(f, ext, exclude_filename, read_folder, current)?;
+            read_recursive(f, ext, exclude_file, read_folder, current)?;
         }
     } else {
         for f in read_folder {
             read_recursive(
                 path.as_ref().join(f),
                 ext,
-                exclude_filename,
+                exclude_file,
                 read_folder,
                 current,
             )?;
@@ -39,11 +39,11 @@ fn read_recursive<P: AsRef<Path>>(
 pub fn read_control<P: AsRef<Path>>(
     path: P,
     ext: &str,
-    exclude_filename: &[String],
+    exclude_file: &[String],
     read_folder: &[String],
 ) -> io::Result<Vec<String>> {
     let mut current = Vec::new();
-    read_recursive(&path, ext, exclude_filename, read_folder, &mut current)?;
+    read_recursive(&path, ext, exclude_file, read_folder, &mut current)?;
 
     Ok(current)
 }
--- src/read/dir.rs
+++ src/read/dir.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 pub fn read_dir<P: AsRef<Path>>(
     path: P,
     ext: &str,
-    exclude_filename: &[String],
+    exclude_file: &[String],
 ) -> io::Result<(Vec<String>, Vec<String>)> {
     let mut folder = Vec::new();
     Ok((
@@ -18,7 +18,7 @@ pub fn read_dir<P: AsRef<Path>>(
                 let filepath = path.as_ref().join(&filename).to_string_lossy().into_owned();
                 if entry.file_type().ok()?.is_file()
                     && Path::new(&filename).extension() == Some(OsStr::new(ext))
-                    && !exclude_filename.iter().any(|f| filename.starts_with(f))
+                    && !exclude_file.iter().any(|f| filename.starts_with(f))
                 {
                     Some(filepath)
                 } else if entry.file_type().ok()?.is_dir() {
