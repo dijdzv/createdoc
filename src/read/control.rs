16bdb16c2d3329f5c0961102a068b5ec2fed7ca8
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-17 19:18:06 +0900

    再帰指定

--- src/read/control.rs
+++ src/read/control.rs
@@ -9,12 +9,19 @@ fn read_recursive<P: AsRef<Path>>(
     read_folder: &[String],
     current: &mut Vec<String>,
 ) -> io::Result<()> {
+    if !path.as_ref().exists() {
+        return Ok(());
+    }
     let (mut read_filename, exist_folder) = read_dir(&path, ext, exclude_filename)?;
     current.append(&mut read_filename);
     if exist_folder.is_empty() {
         return Ok(());
     }
-    if !read_folder.contains(&"*".to_string()) {
+    if read_folder.contains(&"*".to_string()) {
+        for f in &exist_folder {
+            read_recursive(f, ext, exclude_filename, read_folder, current)?;
+        }
+    } else {
         for f in read_folder {
             read_recursive(
                 path.as_ref().join(f),
@@ -23,10 +30,6 @@ fn read_recursive<P: AsRef<Path>>(
                 read_folder,
                 current,
             )?;
-        }
-    } else {
-        for f in &exist_folder {
-            read_recursive(f, ext, exclude_filename, read_folder, current)?;
         }
     }
 
