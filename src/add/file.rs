526902568ad9b835691651111b79e65d0d6ba6d9
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-20 01:38:23 +0900

    with_context

--- src/add/file.rs
+++ src/add/file.rs
@@ -1,4 +1,4 @@
-use anyhow::anyhow;
+use anyhow::Context;
 use std::path::Path;
 
 use crate::error::ErrorMsg;
@@ -8,20 +8,20 @@ pub fn add_file<P: AsRef<Path>>(read_data: &mut ReadData, filepath: P) -> anyhow
     let filename = filepath
         .as_ref()
         .file_name()
-        .ok_or_else(|| anyhow!(ErrorMsg::FileName.as_str()))?
+        .with_context(|| ErrorMsg::FileName.as_str())?
         .to_string_lossy()
         .into_owned();
     let parent_filepath = filepath
         .as_ref()
         .parent()
-        .ok_or_else(|| anyhow!(ErrorMsg::Parent.as_str()))?;
+        .with_context(|| ErrorMsg::Parent.as_str())?;
     if parent_filepath == Path::new(&read_data.read_dir) {
         read_data.push_dir_vec(filename);
     } else {
         let parent_name = parent_filepath
             .strip_prefix(Path::new(&read_data.read_dir))?
             .to_str()
-            .ok_or_else(|| anyhow!(ErrorMsg::ToStr.as_str()))?
+            .with_context(|| ErrorMsg::ToStr.as_str())?
             .replace('\\', "::")
             .replace('/', "::");
         read_data.push_dir_vec(format!("{}::{}", parent_name, filename));
