ed531b39dd6edce66bc588a7dfd8e7289f40f344
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-20 02:00:50 +0900

    error.newしているのを、bailで返して終わり

--- src/tml/read.rs
+++ src/tml/read.rs
@@ -1,24 +1,17 @@
 use crate::tml;
+use anyhow::bail;
 use createdoc::Setting;
-use std::{
-    fs::read_to_string,
-    io::{self, ErrorKind},
-    path::Path,
-};
+use std::{fs::read_to_string, io::ErrorKind, path::Path};
 
-pub fn read_toml() -> Result<Setting, io::Error> {
+pub fn read_toml() -> anyhow::Result<Setting> {
     let result = read_to_string(tml::TOML_PATH);
     let s = match result {
         Ok(s) => s,
         Err(e) if e.kind() == ErrorKind::NotFound => {
             tml::create_toml(tml::TOML, Path::new(tml::TOML_PATH))?;
-            let new_e = io::Error::new(
-                ErrorKind::NotFound,
-                "The `setting.toml` file did not exist, so I created a new one.",
-            );
-            return Err(new_e);
+            bail!("The `setting.toml` file did not exist, so I created a new one.");
         }
-        Err(e) => return Err(e),
+        Err(e) => bail!(e),
     };
 
     let setting = toml::from_str(&s)?;
