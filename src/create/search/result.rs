cf1f96be9d4150cf40efd2bd155f1c8ce2d20932
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-21 09:54:33 +0900

    検索が動かなくなっていたのを改善

--- src/create/search/result.rs
+++ src/create/search/result.rs
@@ -4,14 +4,14 @@ pub fn search_result(output: &mut Output, search_data: &Vec<(&str, Vec<&str>)>) 
     output.add(r#"<div class="search-result"><ul>"#);
     for (filename, v) in search_data {
         output.add(format!(
-            "<a href=\"#{}\"><li class=\"search-list dn\">{}</li></a>",
+            "<a href=\"#f-{}\"><li class=\"search-list dn\">{}</li></a>",
             filename, filename
         ));
 
         for target_name in v {
             output.add(
                 format!(
-                    "<a href=\"#{}\"><li class=\"search-list dn\">{}<span class=\"s-target_name\">{}</span></li></a>",
+                    "<a href=\"#t-{}\"><li class=\"search-list dn\">{}<span class=\"s-target_name\">{}</span></li></a>",
                     target_name, filename, target_name
                 )
             );
