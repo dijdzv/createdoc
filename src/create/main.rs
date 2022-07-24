8a1c55504d3c519a02f05e60ca58bc20fbe7d367
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-24 20:25:26 +0900

    categorizedに対応

--- src/create/main.rs
+++ src/create/main.rs
@@ -39,7 +39,7 @@ pub fn create_main(
 
     output.add("</div>");
 
-    for (filename, file_vec) in all {
+    for (filename, syntax_hah) in categorized {
         // m-file
         output.add(format!("<div class=\"m-file m-{}\">", filename));
 
@@ -55,78 +55,78 @@ pub fn create_main(
             stem_name, stem_name, stem_name
         ));
 
-        for (syntax, target_name, doc, content) in file_vec {
-            // .pair
-            output.add(r#"<div class="pair">"#);
+        for (syntax, target_vec) in syntax_hah {
+            for &(target_name, doc, content) in target_vec {
+                // .pair
+                output.add(r#"<div class="pair">"#);
 
-            // target name
-            output.add(
+                // target name
+                output.add(
                 format!(
                     "<h3 class=\"m-target_name\" id=\"t-{0}-{1}\">
                     <a href=\"#t-{0}-{1}\">{1}</a><input type=\"text\" class=\"hidden-input\" value=\"{1}\">
                     <i class=\"gg-copy\"></i><i class=\"gg-check dn\"></i>
                     </h3>",
                     syntax,target_name
-                )
-            );
+                ));
 
-            // docコメント
-            output.add(r#"<pre class="doc"><p class="doc-p">"#);
-            let re_space = Regex::new(r"[\s\t]+")?;
-            let re_tag = Regex::new(r"@[a-zA-Z]+")?;
-            let re_type = Regex::new(r#"(array|int(eger)?|float|string|bool(ean)?|void)[^\s]*"#)?;
-            for d in doc {
-                // 先頭からtrim
-                let d = d.trim_start_matches(constant::TRIM_PATTERN);
-                // ２つ以上連続する空白を1つに
-                let d = re_space.replace_all(d, " ").into_owned();
-                // tagをspanで囲う
-                let tag = re_tag.find(&d);
-                let d = match tag {
-                    Some(t) => {
-                        format!(
-                            "{}<span class=\"tag\">{}</span>{}",
-                            &d[..t.start()],
-                            &d[t.start()..t.end()],
-                            &d[t.end()..]
-                        )
-                    }
-                    None => d,
-                };
-                let typ = re_type.find(&d);
-                let d = match typ {
-                    Some(t) => {
-                        format!(
-                            "{}<span class=\"type\">{}</span>{}",
-                            &d[..t.start()],
-                            &d[t.start()..t.end()],
-                            &d[t.end()..]
-                        )
-                    }
-                    None => d,
-                };
-                output.add(format!("{}\n", d));
+                // docコメント
+                output.add(r#"<pre class="doc"><p class="doc-p">"#);
+                let re_space = Regex::new(r"[\s\t]+")?;
+                let re_tag = Regex::new(r"@[a-zA-Z]+")?;
+                let re_type =
+                    Regex::new(r#"(array|int(eger)?|float|string|bool(ean)?|void)[^\s]*"#)?;
+                for d in doc {
+                    // 先頭からtrim
+                    let d = d.trim_start_matches(constant::TRIM_PATTERN);
+                    // ２つ以上連続する空白を1つに
+                    let d = re_space.replace_all(d, " ").into_owned();
+                    // tagをspanで囲う
+                    let tag = re_tag.find(&d);
+                    let d = match tag {
+                        Some(t) => {
+                            format!(
+                                "{}<span class=\"tag\">{}</span>{}",
+                                &d[..t.start()],
+                                &d[t.start()..t.end()],
+                                &d[t.end()..]
+                            )
+                        }
+                        None => d,
+                    };
+                    let typ = re_type.find(&d);
+                    let d = match typ {
+                        Some(t) => {
+                            format!(
+                                "{}<span class=\"type\">{}</span>{}",
+                                &d[..t.start()],
+                                &d[t.start()..t.end()],
+                                &d[t.end()..]
+                            )
+                        }
+                        None => d,
+                    };
+                    output.add(format!("{}\n", d));
+                }
+
+                // /docコメント
+                output.add("</p></pre>");
+
+                // pre code
+                output.add(format!(
+                    "<pre class=\"code\"><code class=\"language-{}\">",
+                    read_lang
+                ));
+                for c in content {
+                    output.add(format!("{}\n", c));
+                }
+                // /code /pre
+                output.add("</code></pre>");
+
+                // /.pair
+                output.add("</div>");
             }
-
-            // /docコメント
-            output.add("</p></pre>");
-
-            // pre code
-            output.add(format!(
-                "<pre class=\"code\"><code class=\"language-{}\">",
-                read_lang
-            ));
-            for c in content {
-                output.add(format!("{}\n", c));
-            }
-            // /code /pre
-            output.add("</code></pre>");
-
-            // /.pair
-            output.add("</div>");
         }
-        // /m-file
-        output.add("</div>");
     }
 
     // /main
