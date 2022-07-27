b11eb0c644b8be6c04d2d4fd7513f9daaab66ad9
shsyss <103440382+shsyss@users.noreply.github.com>
2022-07-27 14:35:58 +0900

    色付け

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,16 @@ use createdoc::ReadData;
 
 fn main() {
     match app() {
-        Ok(create_filepath) => println!("Created `{}` successfully!", create_filepath),
-        Err(e) => {
-            eprintln!("error: {}", e)
+        Ok(create_filepath) => {
+            println!("\x1b[32mCreated `{}` successfully!\x1b[m", create_filepath)
         }
+        Err(e) => eprintln!("\x1b[31merror:\x1b[m {}", e),
     }
 }
 
 fn app() -> anyhow::Result<String> {
     let setting = tml::read_toml()?;
     let filepaths = read::read_control(&setting)?;
-
     let mut read_data = ReadData::new(&setting);
 
     add::add_control(filepaths, &mut read_data)?;
